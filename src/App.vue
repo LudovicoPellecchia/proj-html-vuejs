475af2c7ac814e8e67f29cf7927a9aa4f5b28425
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-09-04 16:23:14 +0200

    modifiche style scrollbar sito

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,18 @@ export default{
 </template>
 
 <style >
+
+::-webkit-scrollbar{
+    background: rgba(255, 255, 255, 0);
+}
+::-webkit-scrollbar-thumb{
+    border-radius: 100px;
+    background-color: rgb(195, 195, 195);
+
+}
+
+::-webkit-scrollbar-thumb:hover{
+  background-color: #525151;
+}
+
 </style>
